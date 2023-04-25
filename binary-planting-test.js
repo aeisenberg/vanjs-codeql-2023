cf90a274410ba3f02eb94ea7a596bdcc30b3bb8b
Andrew Eisenberg <aeisenberg@github.com>
2023-04-25 16:29:53 -0700

    Clean javascript

--- binary-planting-test.js
+++ binary-planting-test.js
@@ -1,19 +1,15 @@
 import * as toolrunner from "@actions/exec/lib/toolrunner";
 import * as safeWhich from "@chrisgavin/safe-which";
 
-
-
-const binary = await safeWhich.safeWhich("binary");
+const safeBinary = await safeWhich.safeWhich("binary");
 const unsafeBinary = "unsafe-binary";
 
-
-
-// This is a warning
-await new toolrunner.ToolRunner(getBinaries()[0], ["arg1", "arg2"]).exec();
-
 // This is safe
-await new toolrunner.ToolRunner(getBinaries()[1], ["arg1", "arg2"]).exec();
+await new toolrunner.ToolRunner(getBinaries()[0]).exec();
+
+// This is unsafe
+await new toolrunner.ToolRunner(getBinaries()[1]).exec();
 
 function getBinaries() {
-  return [binary, unsafeBinary];
+  return [safeBinary, unsafeBinary];
 }
